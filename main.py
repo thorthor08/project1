dba8975c24ff234b2c7f3dfa41b4f5ff87659313
thor <145383756+thorthor08@users.noreply.github.com>
2024-01-29 13:24:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import streamlit as st
 
 # Main page title
 st.title("Main Page")
-
-# Button to show/hide sidebar content
-if st.button("Show Sidebar Content"):
-    st.sidebar.title("Sidebar")
-    st.sidebar.write("This is some content in the sidebar.")
-    st.sidebar.slider("Slider in sidebar", 0, 100)
-elif st.button("Hide Sidebar Content"):
-    st.sidebar.empty()
+st.sidebar.title("Sidebar")
+st.sidebar.write("boulders")
+st.sidebar.write("flashes")
+st.sidebar.write("create")
