3edb63bd236dd8c75b9a762123aadf909a2f2f20
thor <145383756+thorthor08@users.noreply.github.com>
2024-01-29 13:27:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import streamlit as st
 
 # Main page title
 st.title("Main Page")
+
+# Adding elements to the sidebar with separation lines
 st.sidebar.title("Sidebar")
 st.sidebar.write("boulders")
+st.sidebar.markdown("---")  # Horizontal line for separation
 st.sidebar.write("flashes")
+st.sidebar.markdown("---")  # Another horizontal line for separation
 st.sidebar.write("create")
+6
